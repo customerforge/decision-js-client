46c2f8c3525dbecfceaeb6a87169b69f8342b8cc
Leo Levins <leo.levins@proton.me>
2023-07-17 11:43:00 +1000

    anonymous users are welcome

--- src/profile.js
+++ src/profile.js
@@ -4,14 +4,11 @@ const Profile = function(profileId) {
   this.profileId = profileId
 }
 Profile.prototype.identify = async function(user_id, segment, user_properties) {
-  if(!user_id) {
-    throw new Error("user_id is missing")
-  }
  const profile = await this.httpClient({
     url: 'profiles/identify',
     method: 'POST',
     body: {
-      user_id,
+      ...(user_id ? {user_id} : null)
       user_properties,
       ...(segment ? {segment} : null)
     }
